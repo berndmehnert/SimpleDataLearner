08671673b1c7686cf4261104538368aace996bc3
bernd.h.mehnert <bernd.h.mehnert@gmail.com>
2021-04-14 21:13:41 +0200

    added basic construct

--- src/SimpleDataLearner.jl
+++ src/SimpleDataLearner.jl
@@ -1,5 +1,9 @@
 module SimpleDataLearner
+import Base.map
+using LinearAlgebra
+mutable struct Approximation
+    W; b
+end
 
-greet() = print("Hello World!")
-
+map(f :: Approximation, X) = f.W * X + f.b
 end # module
