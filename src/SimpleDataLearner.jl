8c03976ee2223565352344c4d7cf144de875ff80
bernd.h.mehnert <bernd.h.mehnert@gmail.com>
2021-04-14 21:31:47 +0200

    added lean function skeleton

--- src/SimpleDataLearner.jl
+++ src/SimpleDataLearner.jl
@@ -1,9 +1,16 @@
 module SimpleDataLearner
 import Base.map
 using LinearAlgebra
-mutable struct Approximation
+mutable struct AffineApproximation
     W; b
 end
+struct Datum
+    X; Y
+end
 
-map(f :: Approximation, X) = f.W * X + f.b
+map(f :: AffineApproximation, X) = f.W * X + f.b
+
+function learn!(f :: AffineApproximation, data :: Array{Datum})
+end
+
 end # module
