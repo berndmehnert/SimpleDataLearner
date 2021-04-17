4701aeb6e56993f0541dc8ef3c243f6989029b7a
bernd.h.mehnert <bernd.h.mehnert@gmail.com>
2021-04-17 14:29:11 +0200

    added affine loss function

--- src/SimpleDataLearner.jl
+++ src/SimpleDataLearner.jl
@@ -1,15 +1,29 @@
 module SimpleDataLearner
+
 import Base.map
 using LinearAlgebra
+using ForwardDiff
 
-mutable struct AffineApproximation
+""" 
+Basic differential operators we need:
+"""
+∇(f, x) = ForwardDiff.gradient(f, x)
+
+abstract type Model end
+mutable struct AffineModel <: Model
     W; b
 end
-struct Datum
+
+struct TrainingExample
     X; Y
 end
 
-map(f :: AffineApproximation, X) = f.W * X + f.b
+map(model :: AffineModel, X) = model.W * X + model.b
+
+function Loss(model :: AffineModel, t :: TrainingExample) 
+    α = map(model, t.X) - t.Y
+    return α^2
+end 
 
 function learn!(f :: AffineApproximation, data :: Array{Datum})
 end
