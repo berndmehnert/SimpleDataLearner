73d119fb9068b60b2ceeab5535af8a0b6ff1d36f
bernd.h.mehnert <bernd.h.mehnert@gmail.com>
2021-04-18 13:58:27 +0200

    small change

--- src/SimpleDataLearner.jl
+++ src/SimpleDataLearner.jl
@@ -1,8 +1,9 @@
 module SimpleDataLearner
 
-import Base.map
 using LinearAlgebra
 using ForwardDiff
+
+import Base.map
  
 # Basic differential operators we need:
 ∇(f, x) = ForwardDiff.gradient(f, x)
@@ -18,18 +19,16 @@ end
 
 map(model :: AffineModel, X) = model.W * X + model.b
 
-function quadraticLoss(model :: Model, o :: Observation) 
-    α = map(model, o.X) - o.Y
-    return dot(α, α)
-end
-
+"""
+For the moment, perform a gradient step with respect to the quadratic loss. We generalize later.
+"""
 function gradientStep!(model :: AffineModel, o :: Observation, η :: Float64) :: AffineModel
-    α = quadraticLoss(model, o)
+    α = map(model, o.X) - o.Y
     model.W = model.W - 2*η*o.X*α
     model.b = model.b - 2*η*α
 end
 
-function learn!(model :: Model, loss, data :: Array{Observation})
+function fit!(model :: Model, observations :: Array{Observation})
 end
 
 end # module
